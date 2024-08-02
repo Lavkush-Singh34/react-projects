e7ab7e76420a2cdf3f496c74a4c0327f4c3e3e45
lavkush <lavkushsingh1998@gmail.com>
2024-08-02 17:25:57 +0530

    first commit

---
+++ counter-in-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+  
+  const addValue = () => {
+    counter < 20 && setCounter(counter + 1)
+  }
+  
+  const removeValue = () => {
+    counter > 0 && setCounter(counter - 1)
+  }
+
+
+  return (
+    <>
+    <h1>Value : {counter} </h1>
+    <button onClick={addValue}>Add Value</button> <hr />
+    <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
